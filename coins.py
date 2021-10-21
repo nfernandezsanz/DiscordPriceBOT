7947685b64ab3d647ae27e4a0948098a7347e0a3
nfernandezsanz <nfernndezsanz@frba.utn.edu.ar>
2021-10-20 23:53:12 -0300

    V1.1

--- coins.py
+++ coins.py
@@ -9,8 +9,8 @@ def get_coin_status(id):
     api_result = cg.get_price(ids=id, vs_currencies='usd', include_24hr_vol='true',include_24hr_change='true', include_last_updated_at='true')
     response   = dict()
     response['Price']  = api_result['gamestarter']['usd']
-    response['Change'] = api_result['gamestarter']['usd_24h_change']
-    response['Vol']    = api_result['gamestarter']['usd_24h_vol']
+    response['Change'] = round(api_result['gamestarter']['usd_24h_change'],2)
+    response['Vol']    = round(api_result['gamestarter']['usd_24h_vol'],1)
     return response
 
-print(get_coin_status('gamestarter'))
+#print(get_coin_status('gamestarter'))
--- main.py
+++ main.py
@@ -1,45 +1,32 @@
 import asyncio
 import discord
-
+import os
 from discord.ext import commands
-
-from pycoingecko import CoinGeckoAPI
-import requests
-import json
-
-
+from coins       import *
+from dotenv      import load_dotenv, find_dotenv
 
 bot1 = commands.Bot(command_prefix='#')
 bot2 = commands.Bot(command_prefix='$')
 
-cg = CoinGeckoAPI()
+#Config
+load_dotenv(find_dotenv())
+Token_1 = os.environ.get("Token_1")
+Token_2 = os.environ.get("Token_2")
 
 
-'''
-class Tracker(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # create the background task and run it in the background
-        self.bg_task = self.loop.create_task(self.my_background_task())
-
-    async def on_ready(self):
-        print(f'Inicio de sesion como {self.user} (ID: {self.user.id})')
-        print('------')
-
-    async def my_background_task(self):
-        await self.wait_until_ready()
-        counter = 0
-        while not self.is_closed():
-            counter += 1
-            await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= str(counter) + "USD"))
-            await asyncio.sleep(60) # task runs every 60 seconds
-'''
 async def update_task(bot, coin):
     counter = 0
     while(True):
         counter += 1
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= str(counter) + " USD"))
+        data  = get_coin_status('gamestarter')
+        emoji = "➡️"
+
+        if(data['Change'] < 0):
+            emoji = "⏬"
+        elif(data['Change'] > 0):
+            emoji = "⏫"
+
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name= "$" + str(data['Price']) + " " + emoji + " " + str(data['Change']) + "%"))
         await asyncio.sleep(10)
 
 @bot1.event
@@ -56,10 +43,10 @@ async def on_ready():
     await bot2.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="0 USD"))
 
 
-# Inicializo los Bots
+# Initialize Bots
 loop  = asyncio.get_event_loop()
-task1 = loop.create_task(bot1.start('OTAwNTI3MDU0Mjk3MTE2NzAy.YXCnLA.g6dMryNi8IWVnWu3YsMY-_1J0NU')) #Jr
-task2 = loop.create_task(bot2.start('OTAwNTIyNzY5NTYzOTM0NzQw.YXCjLw.Bwvjq3wm9bAkN5a7ecciuHA2y0o')) #Tipico
+task1 = loop.create_task(bot1.start(Token_1)) #GAME TOKEN BOT
+task2 = loop.create_task(bot2.start(Token_2)) #DARK TOKEN BOT
 
 try:
     loop.run_forever()
